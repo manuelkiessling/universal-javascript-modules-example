c0c577db2deef019e45af231a581e8845328a09c
Manuel Kiessling <manuel@kiessling.net>
2012-04-03 12:38:21 +0200

    refactored to a RequireJS pattern that also works with specs in subdirs

--- spec/multiply.spec.js
+++ spec/multiply.spec.js
@@ -1,7 +1,9 @@
-var multiply = require("../lib/multiply.js");
+if (typeof define !== 'function') { var define = require('amdefine')(module) }
 
-describe("multiply", function() {
-  it("multiplies two numbers", function() {
-    expect(multiply(2, 5)).toEqual(10);
+define(["../lib/multiply.js"], function(multiply) {
+  describe("multiply", function() {
+    it("multiplies two numbers", function() {
+      expect(multiply(2, 5)).toEqual(10);
+    });
   });
 });
